417760aaaac2c961fccf286c8d43f1636f3ff15c
Shlok Kulkarni <shlok.kulkarni@elastic.run>
2023-10-01 15:29:32 +0530

    Data for mustard and notebook for analysis

--- BERT-base/env_create.sh
+++ BERT-base/env_create.sh
@@ -3,8 +3,9 @@
 echo "Making python virtual environment"
 python3 -m venv BERT_base_env
 
+source BERT_base_env/bin/activate
+pip install --upgrade pip
+
 echo "Installing packages"
 # Activate the virtual environment
 pip3 install -r requirements.txt
-
-
